3bd9755a0e77277f5eeffca158baeb655ee63315
fukenrice <art.podoroga@yandex.ru>
2024-01-03 04:56:11 +0300

    Add auth persistence

--- firebase/config.ts
+++ firebase/config.ts
@@ -2,9 +2,13 @@ import {getFirestore, doc, setDoc, getDoc, deleteDoc, collection} from "firebase
 import 'firebase/compat/auth';
 import firebase from 'firebase/compat/app';
 import {firebaseConfig} from "./secrets";
-import { GoogleAuthProvider } from "firebase/auth";
+import {getReactNativePersistence, initializeAuth} from "firebase/auth"
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const app = firebase.apps.length === 0 ? firebase.initializeApp(firebaseConfig) : firebase.app()
+initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+});
 const auth = firebase.auth()
 const db = getFirestore(app)
 
