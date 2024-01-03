74cd31d9ba9317d97f8a4cb37349d9fc94d526b8
fukenrice <art.podoroga@yandex.ru>
2024-01-03 04:01:05 +0300

    Add main/auth flow logic

--- Navigate.tsx
+++ Navigate.tsx
@@ -1,6 +1,11 @@
 import {createStackNavigator} from "@react-navigation/stack";
 import AuthScreen from "./ui/auth/screens/AuthScreen"
 import {NavigationContainer} from "@react-navigation/native";
+import {auth} from "./firebase/config";
+import MainScreen from "./ui/main/screens/MainScreen";
+import {useEffect, useState} from "react";
+import firebase from "firebase/compat";
+import User = firebase.User;
 
 const commonOptions = {
     headerShown: false,
@@ -12,10 +17,31 @@ const loginOptions = {
 
 const MainStack = createStackNavigator()
 
+
+
 export default function Navigate() {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        return auth.onAuthStateChanged((user) => {
+            setUser(user);
+        });
+    }, []);
+
+
     return <NavigationContainer>
         <MainStack.Navigator>
-            <MainStack.Screen name="Login" component={AuthScreen} options={loginOptions}/>
+            {user === null ?
+                <>
+                    <MainStack.Screen name="Login" component={AuthScreen} options={loginOptions}/>
+                </>
+                :
+                <>
+                    <MainStack.Screen name="Main" component={MainScreen} options={loginOptions}/>
+                </>
+            }
+
+
         </MainStack.Navigator>
     </NavigationContainer>
 }
